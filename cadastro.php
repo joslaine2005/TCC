9082408d14658c3bd8cf19cd3bfb95235b017ed0
joslaine2005 <143111885+joslaine2005@users.noreply.github.com>
2024-08-12 10:19:22 -0300

    Update and rename cadastro.html to cadastro.php

--- cadastro.php
+++ cadastro.php
@@ -1,3 +1,38 @@
+<?php
+
+   if(isset($_POST['submit']))
+   {
+    //print_r('Nome: ' . $_POST['nome']);
+    //print_r('<br>');
+    //print_r('Email:' . $_POST['email']);
+    //print_r('<br>');
+    //print_r('Telefone: ' . $_POST['telefone']);
+    //print_r('<br>');
+    //print_r('Sexo: ' . $_POST['genero']);
+    //print_r('<br>');
+    //print_r('Data_nascimento: ' . $_POST['data_nascimento']);
+    //print_r('<br>');
+    //print_r('Cidade: ' . $_POST['cidade']);
+    //print_r('<br>');
+    //print_r('Estado: ' . $_POST['estado']);
+    //print_r('<br>');
+    //print_r('Endereço: ' . $_POST['endereco']);
+
+    include_once('config.php');
+
+      $nome = $_POST['nome'];
+      $email = $_POST['email'];
+      $telefone = $_POST['telefone'];
+      $sexo = $_POST['genero'];
+      $data_nasc = $_POST['data_nascimento'];
+      $cidade = $_POST['cidade'];
+      $estado = $_POST['estado'];
+      $endereco = $_POST['endereco'];
+
+      $result = mysqli_query($conexao, "INSERT INTO usuarios(nome,email,telefone,sexo,data_nasc,cidade,estado,endereco) VALUES ('$nome', '$email','$telefone','$sexo','$data_nasc',
+      '$cidade','$estado','$endereco')");
+   }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,7 +45,7 @@
 </head>
 <body>
     <div class="box">
-        <form action="">
+        <form action="cadastro.php" method = "POST">
             <fieldset>
                 <legend><b>fórmulario de cliente</b></legend>
                 <br>
